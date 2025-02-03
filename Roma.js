9afb6a87c35a14f57ea34a701dc077b655f8ea1e
moripogi <48934558+moripogi@users.noreply.github.com>
2025-02-04 01:02:08 +0800

    Update Roma.js

--- Roma.js
+++ Roma.js
@@ -1,4 +1,4 @@
-//DASHBOARD
+//DASHBOARD-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 $(document).ready(function(){
   var h = $("h1:eq(0)").text();
   //$("body").append("<h2 class='temps'></h2>");
@@ -166,7 +166,196 @@ $(document).ready(function(){
     observer.observe(document.querySelector(sr),{attributes:true,childList:true,characterData:true});
   }
 });
-//SHORTCUT KEYS
+//MANAGE TOOLS--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+$(document).ready(function(){
+  var h = $("h1:eq(0)").text();
+  var w = window.name;
+  var l;
+  var lob = ["Match","Listing","Suppress","Geocode","Unverified","Clear","Internal","External","Manager","Remove Manager"];
+  function pr(x){
+    for(c=0;c<l;c++){
+      if($(".x:eq("+c+")").prop("checked")){
+        var w = window.open($("a:contains(edit):eq("+c+")").attr("href"),x+c);
+      }
+    }
+  }
+  function ct(x){
+    $.get(x,function(response){
+      var s = $(response).find(".js-duplicate-item").length;
+      $("#sa").append("<tr><td><a href='"+x+"'>"+x+"</a></td><td>"+s+"</td></tr>");
+    });
+  }
+  if(h == "Task Processing Details"){
+    if($("#tasktype").val() == 123 || 1)
+    {
+      var c = $("a:contains(Audit)").length;
+      $("#content").append("<table><tbody id='sa'></tbody></table>");
+      for(i=0;i<c;i++){
+        var j = $("a:contains(Audit):eq("+i+")").attr("href");
+        ct(j);
+      }
+    }
+  }
+  function sw(x){
+    if($("input[value='"+x+"']").is(":checked")){
+      close();
+    }
+    else{
+      $("label input").removeAttr('checked');
+      $("input[value='"+x+"']").click();
+      $("input[value='Save Changes']").click();
+    }
+  }
+  if(h == "Edit User"){
+    window.scrollTo(0,200);
+    if(w.includes("Mat")){
+      sw(88);
+    }
+    if(w.includes("Lis")){
+      sw(89);
+    }
+    if(w.includes("Sup")){
+      sw(123);
+    }
+    if(w.includes("Geo")){
+      sw(124);
+    }
+    if(w.includes("Unv")){
+      sw(126);
+    }
+    if(w.includes("Int")){
+      if($("input[name='userView.isInternal']").is(":checked")){
+        close();
+      }
+      else{
+        $("input[name='userView.isInternal']").click();
+        $("input[value='Save Changes']").click();
+      }
+    }
+    if(w.includes("Ext")){
+      if($("input[name='userView.isInternal']").is(":checked")){
+        $("input[name='userView.isInternal']").click();
+        $("input[value='Save Changes']").click();
+      }
+      else{
+        close();
+      }
+    }
+    if(w.includes("Man")){
+      if($("input[name='userView.isManager']").is(":checked")){
+        close();
+      }
+      else{
+        $("input[name='userView.isManager']").click();
+        $("input[value='Save Changes']").click();
+      }
+    }
+    if(w.includes("Rem")){
+      if($("input[name='userView.isManager']").is(":checked")){
+        $("input[name='userView.isManager']").click();
+        $("input[value='Save Changes']").click();
+      }
+      else{
+        close();
+      }
+    }
+  }
+  if(h == "Users"){
+    if(w.includes("clearTasks")){
+      window.addEventListener("unload",function(){
+        close();
+      });
+      $("form[action='"+w+"'] .link-input").click();
+    }
+    else if($("h2").text() != "1"){
+      $("h2").text("1");
+      $("thead td").attr('colspan',3);
+      $("thead tr").append("<td><input type='checkbox' id='y'><button id='z'>⇌</button></td>");
+      $(".verticalContainer div:eq(0)").remove();
+      l = $("tbody tr").length;
+      var i = 0;
+      $(".clear:eq(1)").remove();
+      while(i<l){
+        var d = $("span:eq("+i+")").text().replace(/\s/g,'');
+        if(am.includes(d)){
+          i++;
+        }
+        else{
+          $("tbody tr:eq("+i+")").remove();
+          l--;
+        }
+      }
+      l = $("tbody tr").length;
+      $("td:contains(password)").remove();
+      $("td:has(.link-input)").hide();
+      $("tbody tr").append("<td><button class='w'>clear</button></td><td><input type='checkbox' class='x'></td>");
+      for(i=0;i<lob.length;i++){
+        $("#content").append("<td><button id='"+lob[i].substring(0,3)+"'>"+lob[i]+"</button></td>");
+      }
+      var t = "tbody:eq(0) .x";
+      $("#y").click(function(){
+        if($("#y").prop("checked")){
+          $(t).prop("checked",true);
+        }
+        else{
+          $(t).prop("checked",false);
+        }
+      });
+      $("#z").click(function(){
+        for(i=0;i<$(t).length;i++){
+          if($(".x:eq("+i+")").prop("checked")){
+            $(".x:eq("+i+")").prop("checked",false);
+          }
+          else{
+            $(".x:eq("+i+")").prop("checked",true);
+          }
+        }
+      });
+      $("#Mat").click(function(){
+        pr("Mat");
+      });
+      $("#Lis").click(function(){
+        pr("Lis");
+      });
+      $("#Sup").click(function(){
+        pr("Sup");
+      });
+      $("#Geo").click(function(){
+        pr("Geo");
+      });
+      $("#Unv").click(function(){
+        pr("Unv");
+      });
+      $("#Int").click(function(){
+        pr("Int");
+      });
+      $("#Ext").click(function(){
+        pr("Ext");
+      });
+      $("#Man").click(function(){
+        pr("Man");
+      });
+      $("#Rem").click(function(){
+        pr("Rem");
+      });
+      $("#Cle").click(function(){
+        for(i=0;i<l;i++){
+          if($(".x:eq("+i+")").prop("checked")){
+            window.open(window.location.href,$("form:has(.link-input):eq("+i+")").attr("action"));
+          }
+        }
+      });
+      $(".w").click(function(){
+        var i = $(".w").index(this);
+        window.open(window.location.href,$("form:has(.link-input):eq("+i+")").attr("action"),'resizable,height=1,width=1');
+      });
+      $("tbody tr>:nth-child(1)").click(function(){
+        $(".x:eq("+$("tbody tr>:nth-child(1)").index(this)+")").click();
+      });
+    }
+  }
+});
+//SHORTCUT KEYS-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 $(document).ready(function(){
   var sl = [];
   var b;
